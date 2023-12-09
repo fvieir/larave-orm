5d0dd950b93c1274d5bdd05306447d8059c40673
Fabricio Dourado <fcvieira2@gmail.com>
2023-12-09 14:00:41 -0300

    Add novas rotas

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,37 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('select', function () {
+    // $user = User::where('id', 101)->firstOrFail();
+
+    // $user = User::where('id', 101)->first;
+    // Combinação de where com fisrt101
+    $user = User::firstWhere('id', '<=', 100);
+
+    dd($user);
+});
+
+Route::get('where', function (User $user) {
+    $users = $user->where('name', '=', request()->get('name'))
+        ->orWhere(function ($query) {
+            $query->where('email', '<>', 'teste@gmail.com');
+            $query->where('email', '<>', 'outro@gmail.com');
+        })
+        ->toSql();
+
+    dd($users);
+});
+
+Route::get('paginacao', function () {
+    $users = User::where('email', 'LIKE', '%@%')->paginate(5);
+
+    return $users;
+});
+
+Route::get('orderby', function () {
+    $users = User::orderBy('name', 'DESC')->get();
+
+    return $users;
 });
